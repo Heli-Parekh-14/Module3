71a0ffa8784739de832835f333794203f2ffc120
Heli Parekh <72486103+Heli-Parekh-14@users.noreply.github.com>
2020-10-26 09:30:35 -0400

    Add files via upload

---
+++ Namespace&Library/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary1;
+using System;
+using CA2 = ConsoleApp2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            //class library[.net library] usage
+            Class1 o = new Class1();        //class 1 belongs to class library 1
+            int res = o.add(x, y);
+            Console.WriteLine(res);
+
+            //namespace usage
+            CA2.Program.display();          //namespace in another solution/project-added reference,using alias=<namspace name>,use the namespace
+
+            Console.Read();
+        }
+    }
+}
